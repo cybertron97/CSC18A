920b09875de5908275a0e91a633a665c377629c7
Tanish Arora <35354810+cybertron97@users.noreply.github.com>
2019-02-02 10:33:35 -0800

    Add files via upload

---
+++ lab#1/HelloDialog.java
@@ -0,0 +1,11 @@
+// Filename HelloDialog.java
+// Written by Tanish
+// Written on 1/5/2017
+
+import javax.swing.JOptionPane;
+public class HelloDialog {
+    public static void main(String[] args)
+{
+JOptionPane.showMessageDialog(null, "Hello, world!");
+}
+}
---
+++ lab#1/hello2.java
@@ -0,0 +1,13 @@
+// Filename Hello2.java
+// Written by Tanish
+// Written on 1/5/2017
+/* This class demonstrates the use of the println()
+method to print the message Hello, world! */
+
+public class hello2 {
+    public static void main(String[] args)
+{
+System.out.println("Hello, world!");
+}
+    
+}
